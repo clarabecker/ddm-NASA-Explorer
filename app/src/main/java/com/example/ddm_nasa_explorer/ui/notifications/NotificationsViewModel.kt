53181e6139eb4b5993dfeff22a3b6c9beea51ef5
CLARA DOS SANTOS BECKER <02178611052@edu.udesc.br>
2025-11-10 14:59:51 -0300

    initial commit

---
+++ app/src/main/java/com/example/ddm_nasa_explorer/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.ddm_nasa_explorer.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
